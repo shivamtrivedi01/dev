0805376d0b704d6d54fb0f747e3329ca11474bdb
Shivam Trivedi <shivamtrivedi908@gmail.com>
2026-09-06 22:20:22 +0530

    automated dev commit

--- env/.config/hypr/monitors.lua
+++ env/.config/hypr/monitors.lua
@@ -1,25 +1,25 @@
 hl.monitor({
-    output = "DP-1",
-    mode = "2560x1440@240",
-    position = "0x0",
-    scale = 1,
+	output = "DP-1",
+	mode = "2560x1440@240",
+	position = "0x0",
+	scale = 1,
 })
 
 hl.monitor({
-    output = "HDMI-A-1",
-    mode = "1920x1080@60",
-    position = "2560x0",
-    scale = 1,
+	output = "HDMI-A-1",
+	mode = "1920x1080@60",
+	position = "2560x0",
+	scale = 1,
 })
 
-hl.workspace({
-    id = 1,
-    monitor = "HDMI-A-1",
-    default = true,
+hl.workspace_rule({
+	workspace = "1",
+	monitor = "HDMI-A-1",
+	default = true,
 })
 
-hl.workspace({
-    id = 2,
-    monitor = "DP-1",
-    default = true,
+hl.workspace_rule({
+	workspace = "2",
+	monitor = "DP-1",
+	default = true,
 })
